dfea0446de34e6548162cea8dfcc7632aa33a026
albina <albinarubydev@gmail.com>
2025-10-22 13:42:30 +0400

    1. добавлен класс TimeHandler; 2. исправлена ошибка: после возврата 404 код продолжал выполняться

--- app.rb
+++ app.rb
@@ -1,30 +1,38 @@
+require_relative 'time_handler'
+
 class App
 
   def call(env)
     request = Rack::Request.new(env)
-
-    not_found unless request.path_info == '/time'
-      
-    #binding.irb
-    #
-    #  если есть - проверяем дальше
-    #
-    #    нужно проверить: это известные форматы или белиберда?
-    #    может создать под это отдельный middleware?
-    #
-    #      если белиберда - вернуть статус - 400, headers, тело - "Unknown time format [epoch]"
-    #      (если неизвестных форматов несколько, 
-    #      все они должны быть перечислены в теле ответа, например: "Unknown time format [epoch, age]")
-    #
-    #      если все норм, 
-    #          готовимся вернуть ответ
-    #          вернуть статус - 200, headers, тело - сформированный ответ
-    #         !они могут быть в любом порядке
-    #
-    
+    return not_found unless request.path_info == '/time'
+    further_processing(request)
   end
 
   private
+
+  def further_processing(request)
+    binding.irb
+    #
+    #нужно проверить: это известные форматы или белиберда?
+    #
+    #если белиберда - вернуть статус - 400, headers, тело - "Unknown time format [epoch]"
+    #(если неизвестных форматов несколько, 
+    #все они должны быть перечислены в теле ответа, например: "Unknown time format [epoch, age]")
+    #
+    #если все норм, 
+    #готовимся вернуть ответ
+    #вернуть статус - 200, headers, тело - сформированный ответ
+    #!они могут быть в любом порядке
+    #
+  end
+
+  def ok
+    response(200, ['надо подготовить'])
+  end
+
+  def unknown_format
+    response(400, ['надо подготовить'])
+  end
 
   def not_found
     response(404, ['Not found'])
---
+++ time_handler.rb
@@ -0,0 +1,18 @@
+class TimeHandler
+  DATETIME_FORMATS = {
+    'year' => '%Y',
+    'month' => '%m',
+    'day' => '%d',
+    'hour' => '%H',
+    'minute' => '%M',
+    'second' => '%S'
+  }.freeze
+
+  def initialize ()
+    
+  end
+
+  def is_the_format_known?
+    
+  end
+end
