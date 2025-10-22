fa0a6f13d2671cd7ef577546a5164d9c840e35c2
albina <albinarubydev@gmail.com>
2025-10-22 13:03:20 +0400

    feat: готов ответ 404

--- app.rb
+++ app.rb
@@ -3,37 +3,38 @@ class App
   def call(env)
     request = Rack::Request.new(env)
 
-    binding.irb
+    not_found unless request.path_info == '/time'
+      
+    #binding.irb
     #
-    #  если нет - вернуть статус - 404, заголовок - text/plain, тело - что-то вроде "not found"
     #  если есть - проверяем дальше
     #
     #    нужно проверить: это известные форматы или белиберда?
     #    может создать под это отдельный middleware?
     #
-    #      если белиберда - вернуть статус - 400, заголовок - text/plain, тело - "Unknown time format [epoch]"
+    #      если белиберда - вернуть статус - 400, headers, тело - "Unknown time format [epoch]"
     #      (если неизвестных форматов несколько, 
     #      все они должны быть перечислены в теле ответа, например: "Unknown time format [epoch, age]")
     #
     #      если все норм, 
     #          готовимся вернуть ответ
-    #          вернуть статус - 200, заголовок - text/plain, тело - сформированный ответ
+    #          вернуть статус - 200, headers, тело - сформированный ответ
     #         !они могут быть в любом порядке
     #
-    [status, headers, body]
+    
   end
 
   private
 
-  def status
-    200
+  def not_found
+    response(404, ['Not found'])
+  end
+
+  def response(status, body)
+    [status, headers, body]
   end
 
   def headers
     { 'content-type' => 'text/plain' }
-  end
-
-  def body
-    ["Welcome abroad!\n"]
   end
 end
